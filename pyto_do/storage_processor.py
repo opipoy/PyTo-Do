bd61aa243d4fafe8e77a233ed2525cf0163dbfe4
Cosmic Lamanite <cosmic.lamanite@gmail.com>
2025-02-13 09:49:30 -0300

    added return and deleted storage.json to always create a new one to start

--- pyto_do/storage_processor.py
+++ pyto_do/storage_processor.py
@@ -8,16 +8,18 @@ def load_tasks():
     try:
         with open(TASKS_FILE, "r") as file:
             tasks = json.load(file)
+            return tasks
     except FileNotFoundError:
         # if file does not exist, return an empty list
         return []
     except json.JSONDecodeError:
         # if file is corrupted, reset tasks
         print("Error: Resetting tasks")
-        return []
+    return []
     
     # Save the tasks to the file
 def save_tasks(tasks):
     with open(TASKS_FILE, "w") as file:
         json.dump(tasks, file, indent=4) # indent for pretty printing
     print("Tasks saved to file")
+    
